83b04feaafa0c4db5b8d8688f4fe0987bb24cbdc
Gustavo <a20192858@pucp.edu.pe>
2025-04-20 14:13:02 -0500

    cambios

--- sitio_web/urls.py
+++ sitio_web/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from dashboard.views import login_view, authorize_view, logout_view, ver_dashboard, dashboard_usuario
+from dashboard.views import login_view, authorize_view, logout_view, ver_dashboard, dashboard_usuario, validar_asistencia, redireccion_por_grupo
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,8 +9,10 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("authorize/", authorize_view, name="authorize"),
     path("logout/", logout_view, name="logout"),
-    path("", ver_dashboard, name="ver_dashboard"),
+    path("", redireccion_por_grupo, name="inicio"),
     path("usuario/", dashboard_usuario, name="dashboard_usuario"),
+    path("usuario/validar-asistencia/", validar_asistencia, name="validar_asistencia"),
+
 ]
 # 👇 Esto permite servir archivos estáticos cuando DEBUG = False
 if not settings.DEBUG:
